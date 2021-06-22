3872db6807265d5d8cfe6a55c107f8d5223fc86e
Fredson Chaves <fredsonchaves07@gmail.com>
2021-06-22 12:39:13 -0300

    feat: placeholder with pass and dot

---
+++ fundamentals/passanddot.py
@@ -0,0 +1,13 @@
+"""
+- Dentro dos blocos de funções ou if, while podemos utilizar o comando 
+`pass` como placeholder para futuras implementações
+- Podemos utilizar elipse que possui a mesma função do `pass`
+"""
+
+# Utilizando o pass
+if True:
+    pass
+
+# Utilziando elipse
+if True:
+    ...
