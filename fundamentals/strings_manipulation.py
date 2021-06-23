23531def2ddcbcbe3268f3f08a16e17a5b1a3d48
Fredson Chaves <fredsonchaves07@gmail.com>
2021-06-23 09:00:47 -0300

    feat: string manipulation with python

---
+++ fundamentals/strings_manipulation.py
@@ -0,0 +1,9 @@
+""" 
+- Strings são arrays com indices de caracteres
+- Fatiamento é a técnica de manipulação de strings ([:indice])
+"""
+
+text = 'Python'
+# Acessando o indice da string 'P'
+print(text[0])
+print(text[:2])
