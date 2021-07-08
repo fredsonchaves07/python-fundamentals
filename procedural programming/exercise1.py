7d027a937d32417317a516280a42433f82599502
Fredson Chaves <fredsonchaves07@gmail.com>
2021-07-08 08:22:01 -0300

    feat: formating code with black

--- procedural programming/exercise1.py
+++ procedural programming/exercise1.py
@@ -2,19 +2,20 @@ def duplicate_number(number_list: list):
     set_number = set()
 
     for number in number_list:
-       if number in set_number:
-           return number
-       
-       set_number.add(number)
+        if number in set_number:
+            return number
 
-    return -1 
+        set_number.add(number)
+
+    return -1
+
 
 integer_numbers_list = [
     [1, 10, 2, 5, 6, 10, 2, 1, 3, 10],
     [2, 5, 3, 4, 2, 1, 9, 1, 9, 8, 6],
-    [5, 7, 1, 1, 5, 9, 4, 3, 2, 1], 
+    [5, 7, 1, 1, 5, 9, 4, 3, 2, 1],
     [10, 6, 5, 7, 4, 6, 5, 9, 8, 10],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 ]
 
 for number_list in integer_numbers_list:
