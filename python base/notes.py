ebcbcb853609f05c3f21eb4d10df52c359f09b33
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-08-03 19:01:24 -0300

    feat: script de geracao de notas

--- python base/notes.py
+++ python base/notes.py
@@ -4,7 +4,7 @@ tag: tecnologia
 text:
 Anotação geral sobre carreira de tecnologia
 
-$ notes.py read --tag=tech
+$ notes.py read tech
 ..
 ...
 """
@@ -15,24 +15,33 @@ import sys
 
 path = os.curdir
 filepath = os.path.join(path, "notes.txt")
+cmds = ("read", "new")
 
 arguments = sys.argv[1:]
 if not arguments:
     print("Invalid usage")
+    print(f"You must specify subcommand {cmds}")
     sys.exit(1)
 
-cmds = ("read", "new")
 if arguments[0] not in cmds:
     print("Invalid command {arguments[0}")
 
 if arguments[0] == "read":
-    # Leitura das notas
-    pass
+    for line in open(filepath):
+        titulo, tag, texto = line.split("\t")
+        if tag == arguments[1].lower():
+            print(f"Título: {titulo}")
+            print(f"Texto: {texto}")
+            print("-" * 30)
+            print()
 
 if arguments[0] == "new":
     titulo = arguments[1] #TODO -> Tratar exception
     texto = [
-        f"{titulo}\n",
-        input("tag:").strip(),
+        f"{titulo}",
+        input("tag: ").strip(),
         input("text:\n").strip(),
     ]
+
+    with open(filepath, "a") as file_:
+        file_.write("\t".join(texto) + "\n")
